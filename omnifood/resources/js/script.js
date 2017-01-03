64b6a9a81d23578be3369f761768e3d658c6c612
Archanaa Sathyanarayanan <Archanaa Sathyanarayanan>
2017-01-02 20:14:12 -0600

    scrollAnimationAndMobileNavigationAdded

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     
+    /* For the sticky navigation */
     $('.js--section-features').waypoint(function(direction) {
         if(direction == "down") {
             $('nav').addClass('sticky');
@@ -9,11 +10,87 @@ $(document).ready(function() {
     }, {
       offset: '60px;'
     });
-   
     
-    /*var waypoints = $('#handler-first').waypoint(function(direction) {
-      notify(this.element.id + ' hit 25% from top of window') 
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    /* Navigation scroll */
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+    
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {   
+        if(direction == "down") {
+            $('.js--wp-1').addClass('animated fadeIn');
+        } else {
+            $('.js--wp-1').removeClass('animated fadeIn');
+        }          
     }, {
-      offset: '25%'
-    })*/
+      offset: '50%'
+    });
+    
+     $('.js--wp-2').waypoint(function(direction) {
+        if(direction == "down") {
+            $('.js--wp-2').addClass('animated fadeInUp');
+        } else {
+            $('.js--wp-2').removeClass('animated fadeInUp');
+        } 
+        
+    }, {
+      offset: '50%'
+    });
+    
+     $('.js--wp-3').waypoint(function(direction) {
+        if(direction == "down") {
+            $('.js--wp-3').addClass('animated fadeInLeft');
+        } else {
+            $('.js--wp-3').removeClass('animated fadeInLeft');
+        }         
+    }, {
+      offset: '50%'
+    });
+    
+     $('.js--wp-4').waypoint(function(direction) {
+        if(direction == "down") {
+            $('.js--wp-4').addClass('animated pulse');
+        } else {
+            $('.js--wp-4').removeClass('animated pulse');
+        }         
+    }, {
+      offset: '50%'
+    });
+    
+    /* Mobile Nav*/
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        nav.slideToggle(200);
+        
+        var icon = $('.js--nav-icon i');        
+        if(icon.hasClass('ion-navicon-round')) {
+            icon.removeClass('ion-navicon-round');
+            icon.addClass('ion-close-round');
+        } else {
+            icon.removeClass('ion-close-round');
+            icon.addClass('ion-navicon-round');
+        }
+    });
+    
 });
